f73f0ef64bc340932f5c3fcefd916a62c59b04dd
Christina Gomzina <chrisgoncharova@yandex.ru>
2023-08-16 21:14:24 +0300

    Adds hash module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
+import { HashService } from '../hash/hash.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private readonly hashService: HashService,
+  ) {}
+
+  async create(dto: CreateUserDto): Promise<User> {
+    return this.userRepository.save({
+      ...dto,
+      password: await this.hashService.hashPassword(dto.password),
+    });
+  }
+}
--- src/wishes/wishes.service.ts
+++ src/wishes/wishes.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Wish } from './entities/wish.entity';
 
 @Injectable()
